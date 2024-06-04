1e5a5e9a8822bebf734be0a45a9a4cd98f44035b
DevMoeOsman <mohammad.osman.dev@gmail.com>
2024-06-04 12:41:38 +0300

    workflows commit

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -14,7 +14,7 @@ it('clears the cookie after signing out', async () => {
     .post('/api/users/signout')
     .send({})
     .expect(200);
-
+    // @ts-ignore
   expect(response.get('Set-Cookie')[0]).toEqual(
     'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'
   );
